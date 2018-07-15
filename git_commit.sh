b88adb4f85e74e1f834e039823c9d0b57b0e0d40
Rishikesh Jha <rishijha424@gmail.com>
2018-07-15 23:25:06 +0530

    Doc changes

---
+++ git_commit.sh
@@ -0,0 +1,2 @@
+git remote add origin git@github.com:Rishi-jha/Data_Structures.git
+git push -u origin drafts
--- linked_list/singlyLinkedList.py
+++ linked_list/singlyLinkedList.py
@@ -22,53 +22,81 @@ class Node(object):
             self.next = Node(value)
 
     def __str__(self):
-        return str(self.value)
+        return "Node({})".format(self.value)
+
+    def __repr__(self):
+        return "Node({})".format(self.value)
 
     def show(self):
         while self.next:
             self.next.show()
-            print(self.value,end=">")
+            print(self.value, end=">")
 
 class SinglyLinkedList(object):
     '''
-    Linked List
+    Singly Linked List Class
     '''
 
     def __init__(self):
+        """
+        Constructor for SinglyLinkedList Class
+        """
         self.head = None
         self.length = 0
 
     def insert(self, value, index=None):
+        """
+        Method to insert in a linked list, you can optionally provide index.
+        If index is not provided it will append new node at the end.
+        insert(value,[index])
+        :param: value: Value to be inserted inside the SinglyLinkedList
+        :type: value: Object
+        :param: index: Optional Index attribute to insert at certain index
+        :type: index: int
+        :raises: IndexError if index provided is greater than the length of SinglyLinkedList
+        :rtype: None
+        """
         if index is None:
-            self.insert_at_end(value)
+            self._insert_at_end(value)
         else:
-            self.insert_at_index(value, index)
+            self._insert_at_index(value, index)
+
     def __iter__(self):
         node = self.head
         while node:
             yield node
             node = node.next
 
-    def insert_at_end(self, value):
+    def _insert_at_end(self, value):
+        """
+        Intermediate method to add a node to end.
+        :param: value: Value to add
+        :type: value: object
+        """
         if self.head:
             self.head.insert(value)
         else:
             self.head = Node(value)
         self.length += 1
 
-    def insert_at_index(self, value, index):
+    def _insert_at_index(self, value, index):
+        """
+        Intermediate method to add a node to end.
+        :param: value: Value to add
+        :type: value: object
+        :param: index: Index to add
+        :type: index: int
+        """
         if self.length <= index:
             raise IndexError("You are trying to insert at index which is not available.\nLenght of List is {0}, your index: {1}".format(self.length, index))
         head = self.head
         if index == 0:
-            print(index)
             self.head = Node(value)
             self.head.next = head
         else:
-            print("asa")
             starting = 0
             while head.next:
-                if starting >= index:
+                if starting == index-1:
                     break
                 head = head.next
                 starting += 1
@@ -81,17 +109,54 @@ class SinglyLinkedList(object):
     def __str__(self):
         return "SinglyLinkedList({})".format([eval(str(i)) for i in self])
 
+    def __repr__(self):
+        return "SinglyLinkedList({})".format([eval(str(i)) for i in self])
+
     def __len__(self):
         return self.length
 
     def show(self, sep="-->"):
-        head = self.head
-        for i in range(0,self.length):
-            if i != 0:
-                print(end=sep)
-            print(head.value,end="")
-            head = head.next
-        print()
+        """
+        Print the complete list in order.
+        :param: sep: Optional Separator: default: -->
+        :type: sep: str
+        :rtype: None
+        """
+        print("{}".format(sep).join([str(item.value) for item in self]))
+    
+    def remove(self,index=0):
+        """
+        Method for removing a Node in the SinglyLinkedList
+        :param: index: Index of the node which you want to delete.
+        :type: index: int
+        :rtype: None
+        :raises ValueError: If trying to delete from an empty list
+        :raises IndexError: If trying to delete which does not exist.
+        """
+        if not self.length:
+            raise ValueError("Cannot delete from an empty Linked List")
+        if not index:
+            self.head = self.head.next
+            self.length -= 1
+        else:
+            self._remove_at_index(index+1)
 
+    def _remove_at_index(self, index):
+        counter = 0
+        curr_node = self.head
+        prev_node = self.head
+        if not (0 < index or index < self.length):
+            raise IndexError("Index you provided does not exist")
+        while curr_node.next != None or counter < index:
+            counter += 1
+            if counter == index:
+                prev_node.next = curr_node.next
+                self.length -= 1
+                return
+            else:
+                prev_node = curr_node
+                curr_node = curr_node.next
 
-
+    def _remove_at_start(self):
+        self.head = self.head.next
+        self.length -= 1
